3152716f4f82660e739585cd5e0293439e06daf4
txtxps@gmail.com <TS-600/g580>
2019-12-25 11:56:14 +0300

    11 12 heroku

--- src/test/java/ru/javawebinar/topjava/web/user/HerokuRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/HerokuRestControllerTest.java
@@ -1,9 +1,14 @@
 package ru.javawebinar.topjava.web.user;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePropertySource;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.util.exception.ErrorType;
 import ru.javawebinar.topjava.web.AbstractControllerTest;
+
+import java.io.IOException;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -16,6 +21,23 @@ class HerokuRestControllerTest extends AbstractControllerTest {
 
     public HerokuRestControllerTest() {
         super(AdminRestController.REST_URL);
+    }
+
+    // Set DATABASE_URL environment for heroku profile
+    static {
+        Resource resource = new ClassPathResource("db/postgres.properties");
+        try {
+            ResourcePropertySource propertySource = new ResourcePropertySource(resource);
+            String herokuDbUrl = String.format("postgres://%s:%s@%s",
+                    propertySource.getProperty("database.username"),
+                    propertySource.getProperty("database.password"),
+                    ((String) propertySource.getProperty("database.url")).substring(18));
+            System.out.println(herokuDbUrl);
+
+            System.setProperty("DATABASE_URL", herokuDbUrl);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     @Test
